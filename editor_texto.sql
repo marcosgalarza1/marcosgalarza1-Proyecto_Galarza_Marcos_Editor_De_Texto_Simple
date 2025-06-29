d27402b8e81d6b3a2f603b89396c12a942771a13
marcosgalarza1 <luismarcogalarza10@gmail.com>
2025-06-29 16:20:50 -0400

    Actualizar `editor_texto.sql` para reflejar el nuevo tiempo de generación del archivo a las 22:20:24 del 29-06-2025.

--- editor_texto.sql
+++ editor_texto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-06-2025 a las 20:05:50
+-- Tiempo de generación: 29-06-2025 a las 22:20:24
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
